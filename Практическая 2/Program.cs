7e87dd4420493d8581265ecb8f4d7fe19f6b8805
z_moff <zmoffzema@gmail.com>
2024-06-19 23:41:08 +0300

    Добавьте файлы проекта.

---
+++ Практическая 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        double radius = 5.0;
+        double area = CalculateCircleArea(radius);
+        Console.WriteLine("The area of the circle is: " + area);
+    }
+
+    public static double CalculateCircleArea(double radius)
+    {
+        return Math.PI * Math.Pow(radius, 2);
+    }
+}
